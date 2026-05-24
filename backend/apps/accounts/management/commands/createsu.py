c8379d852611fc29d8027c4e1b70045616bedf16
Moustapha <moustaphama305@gmail.com>
2026-05-24 22:58:17 +0600

    Update superuser credentials

--- backend/apps/accounts/management/commands/createsu.py
+++ backend/apps/accounts/management/commands/createsu.py
@@ -4,6 +4,6 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin123")
+        if not User.objects.filter(username="mouled").exists():
+            User.objects.create_superuser("mouled", "mouled@example.com", "mouled1234")
             self.stdout.write("Superuser created")
