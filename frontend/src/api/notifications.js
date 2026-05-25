9a31a0e3bd338407b5fb752f78002fd67d715a8e
Moustapha <moustaphama305@gmail.com>
2026-05-25 19:41:02 +0600

    Fix notifications.js import path

--- frontend/src/api/notifications.js
+++ frontend/src/api/notifications.js
@@ -1,4 +1,4 @@
-import axiosInstance from './axiosInstance';
+import axiosInstance from '../services/axiosInstance';
 
 export const notificationsAPI = {
   getNotifications: () => axiosInstance.get('/notifications/'),
